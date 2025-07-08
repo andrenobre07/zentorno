65982036d59b7ba46382ac3296a1ece45812d880
Andre <andre.nobre.123044@epge.pt>
2025-07-08 22:32:49 +0100

    vercelerro

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,26 @@
-// @ts-check
+import next from '@next/eslint-plugin-next';
+import react from 'eslint-plugin-react';
+import hooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import nextPlugin from '@next/eslint-plugin-next';
-import reactPlugin from 'eslint-plugin-react';
-import hooksPlugin from 'eslint-plugin-react-hooks';
-import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
-
-export default tseslint.config(
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
   {
     files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
     plugins: {
-      '@next/next': nextPlugin,
-      'react': reactPlugin,
-      'react-hooks': hooksPlugin,
-      'jsx-a11y': jsxA11yPlugin,
+      '@next/next': next,
+      'react': react,
+      'react-hooks': hooks,
+      'jsx-a11y': jsxA11y,
     },
     rules: {
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs['core-web-vitals'].rules,
-      ...reactPlugin.configs.recommended.rules,
-      ...hooksPlugin.configs.recommended.rules,
-      ...jsxA11yPlugin.configs.recommended.rules,
+      ...next.configs.recommended.rules,
+      ...next.configs['core-web-vitals'].rules,
+      ...react.configs.recommended.rules,
+      ...hooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
       // Podes adicionar ou alterar regras aqui se quiseres
       // Exemplo: 'react/prop-types': 'off',
-    },
-    languageOptions: {
-        globals: {
-            React: 'readonly',
-        }
     },
     settings: {
       react: {
@@ -36,4 +28,4 @@ export default tseslint.config(
       },
     },
   },
-);
+];
